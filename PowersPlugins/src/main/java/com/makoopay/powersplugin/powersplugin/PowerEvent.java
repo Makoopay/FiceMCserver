e5bf17b65c8ec6d46468fe07bcdf83990edd03e3
Makoopay <75006237+Makoopay@users.noreply.github.com>
2020-11-27 19:17:59 +0700

    Update PowerEvent.java

--- PowersPlugins/src/main/java/com/makoopay/powersplugin/powersplugin/PowerEvent.java
+++ PowersPlugins/src/main/java/com/makoopay/powersplugin/powersplugin/PowerEvent.java
@@ -11,18 +11,58 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PowerEvent implements Listener {
+    
+    
+    @EventHandler
+    public void onJoinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+
+        Objective objective = scoreboard.registerNewObjective("FiceMC", "Point Counter");
+        objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+        objective.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + "§f§l   FicePoints   §f");
+
+        Team team = scoreboard.registerNewTeam("point_counter");
+
+        team.addEntry(ChatColor.WHITE.toString());
+
+        team.setPrefix(ChatColor.RED + "Points: " + ChatColor.WHITE);
+
+        team.setSuffix("0");
+
+        objective.getScore(ChatColor.WHITE.toString()).setScore(1);
+
+        player.setScoreboard(scoreboard);
+    }
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerUse(PlayerInteractEvent event) {
         Player p = event.getPlayer();
 
+
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
-            if (p.getItemInHand().getType() == Material.WOOD_SWORD) {
+
+            if (p.getItemInHand().getType() == Material.YELLOW_FLOWER) {
+
                 p.sendMessage(ChatColor.GREEN + " +1 Points ");
 
-
                 event.getPlayer().spawnParticle(Particle.LAVA, event.getPlayer().getLocation(), 10);
+
+                Scoreboard scoreboard = p.getScoreboard();
+
+                Team team = scoreboard.getTeam("point_counter");
+
+                int points = Integer.parseInt(team.getSuffix());
+
+                team.setSuffix(Integer.toString(points + 1));
+
+                p.getInventory().remove(p.getItemInHand());
+
+
+
             }
         }
     }
+
 }
